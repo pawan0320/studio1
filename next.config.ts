1bf4217bbfd6138679faf3be4554abde5fd23fbb
Pawan Sai Kodali <pawansaikodali1524@gmail.com>
2025-08-24 19:35:51 +0000

    In Drug Discovery with Deep Learning project i'm not getting molecule st

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.chemspider.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
