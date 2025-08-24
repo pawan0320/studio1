5447dc2e5e10f653507b3ed9a54555c23de77e10
Pawan Sai Kodali <pawansaikodali1524@gmail.com>
2025-08-24 06:40:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HeroSection>
    
    wit

--- src/types/spline-viewer.d.ts
+++ src/types/spline-viewer.d.ts
@@ -2,6 +2,7 @@ declare namespace JSX {
     interface IntrinsicElements {
       'spline-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
         url: string;
+        'events-target'?: string;
       };
     }
   }
